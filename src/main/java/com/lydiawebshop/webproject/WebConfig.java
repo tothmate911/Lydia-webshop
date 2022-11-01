1734a5a5fa45e4b804dff72fb21f9fdd04c8ca92
tothmate911 <tothmate911@gmail.com>
2022-11-01 09:58:22 +0100

    Specify CORS allowed origin

--- src/main/java/com/lydiawebshop/webproject/WebConfig.java
+++ src/main/java/com/lydiawebshop/webproject/WebConfig.java
@@ -1,5 +1,7 @@
 package com.lydiawebshop.webproject;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -7,11 +9,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
+@Slf4j
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${frontend.url}")
+    private String frontendUrl;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        log.info("FrontendUrl (CORS allowed origin): " + frontendUrl);
+        registry.addMapping("/**")
+                .allowedOrigins(frontendUrl)
+                .allowedMethods("GET", "POST");
     }
 
 }
